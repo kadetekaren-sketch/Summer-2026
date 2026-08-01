77e18271d3f9c6a047212a8b3797473052b7b4aa
Karen Kadete <kadetekaren@gmail.com>
2026-08-01 14:50:55 +0300

    Add guessing game - first Java project

---
+++ GuessingGame.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int secretNumber = random.nextInt(50) + 1;
+        int guess = 0;
+        int attempts = 0;
+        int maxAttempts = 7;
+
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 50.");
+
+        while (guess != secretNumber ) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+           if (attempts == maxAttempts){
+                System.out.println("Sorry, you've used all your attempts. The number was " + secretNumber);
+            }
+            else if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } 
+            else {
+                System.out.println("Correct! You got it in " + attempts + " attempts!");
+            }
+        }
+
+        scanner.close();
+    }
+}
