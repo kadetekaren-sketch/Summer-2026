aa0d3df7a2b2567192f40e235e4f718569f1ad16
Karen Kadete <kadetekaren@gmail.com>
2026-08-04 22:26:29 +0300

    Add sorting visualiser project

---
+++ SortingVisualiser.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SortingVisualiser {
+
+    static void bubbleSort(int[] array) {
+        int n = array.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // swap
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        
+        System.out.println("Before: " + Arrays.toString(array));
+        bubbleSort(array);
+        System.out.println("After: " + Arrays.toString(array));
+    }
+}
