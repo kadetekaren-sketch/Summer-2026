1641a50c6b3ee16366bc04786fe74cb35116d0dd
Karen Kadete <kadetekaren@gmail.com>
2026-08-08 23:58:54 +0300

    Add selection sort with visualisation

--- SortingVisualiser.java
+++ SortingVisualiser.java
@@ -17,12 +17,36 @@ public class SortingVisualiser {
             }
         }
     }
+    static void selectionSort(int[] array) {
+    int n = array.length;
+    
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        
+        for (int j = i + 1; j < n; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        
+        
+        int temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+        System.out.println(Arrays.toString(array));
+    }
+}
 
     public static void main(String[] args) {
-        int[] array = {64, 34, 25, 12, 22, 11, 90};
-        
-        System.out.println("Before: " + Arrays.toString(array));
-        bubbleSort(array);
-        System.out.println("After: " + Arrays.toString(array));
+        int[] array1 = {64, 34, 25, 12, 22, 11, 90};
+        int[] array2 = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Before: " + Arrays.toString(array1));
+        bubbleSort(array1);
+        System.out.println("After: " + Arrays.toString(array1));
+
+        System.out.println("\n=== Selection Sort ===");
+    System.out.println("Before: " + Arrays.toString(array2));
+    selectionSort(array2);
+    System.out.println("After: " + Arrays.toString(array2));
     }
 }
