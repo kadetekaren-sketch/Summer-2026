b40487f7b79270d7126e4bf0f574d8fcf7f429d7
Karen Kadete <kadetekaren@gmail.com>
2026-08-14 23:44:47 +0300

    Add password strength checker

--- PasswordChecker.java
+++ PasswordChecker.java
@@ -5,7 +5,27 @@ public class PasswordChecker {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a password: ");
         String password = scanner.nextLine();
-        System.out.println("You entered: " + password);
+
+        boolean hasMinLength = password.length() >= 8;
+        boolean hasDigit = false;
+        boolean hasSpecialChar = false;
+
+        String specialChars = "!@#$%^&*()_+-=[]{}";
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isDigit(c)) hasDigit = true;
+            if (specialChars.contains(String.valueOf(c))) hasSpecialChar = true;
+        }
+
+        if (!hasMinLength) {
+            System.out.println("Weak - password must be at least 8 characters");
+        } else if (!hasDigit || !hasSpecialChar) {
+            System.out.println("Medium - add numbers and special characters");
+        } else {
+            System.out.println("Strong password!");
+        }
+
         scanner.close();
     }
 }
