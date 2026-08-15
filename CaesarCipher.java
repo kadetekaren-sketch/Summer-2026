c60af502a7cd7501f23983e1411618307e4f4139
Karen Kadete <kadetekaren@gmail.com>
2026-08-15 23:42:31 +0300

    CaesarCiphipher project

---
+++ CaesarCipher.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word ");
+        String word = scanner.nextLine();
+        System.out.println("Enter shift number ");
+        int number = scanner.nextInt();
+    }
+}
