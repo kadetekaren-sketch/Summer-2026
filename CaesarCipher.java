4455364bf8946466d0dcba4a8df3cf8487d930ff
Karen Kadete <kadetekaren@gmail.com>
2026-08-17 23:00:56 +0300

    improve input prompt

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class CaesarCipher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word ");
+        System.out.print("Enter a word: ");
         String word = scanner.nextLine();
-        System.out.println("Enter shift number ");
+        System.out.print("Enter shift number: ");
         int number = scanner.nextInt();
 
         String encrypted = "";
